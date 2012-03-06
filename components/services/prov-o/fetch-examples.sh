2d82e7b83ae03b0874b0ee7345c98dcb7d6d706e
Tim L <timrdf@gmail.com>
2012-03-05 21:21:02 -0500

    escaping <>

--- components/services/prov-o/fetch-examples.sh
+++ components/services/prov-o/fetch-examples.sh
@@ -1,1 +1,7 @@
+touch beforefetch
 curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
+for ttl in `find . -newer beforefetch`; do
+   cat $ttl | sed 's/</\&lt;/g; s/>/\&gt;/g' > b
+   mv b $ttl
+done
+rm beforefetch
