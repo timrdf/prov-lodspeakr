68d50fe2cfadff75316613f2f0268ccdace530d9
Tim L <timrdf@gmail.com>
2012-03-12 17:43:00 -0400

    property info boxes

--- components/services/prov-o/fetch-examples.sh
+++ components/services/prov-o/fetch-examples.sh
@@ -5,17 +5,19 @@ pushd ../../../static/prov-o-diagrams
    curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/involvements.png
 popd
 
-touch beforefetch
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
-# Examples
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-association.ttl
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-derivation.ttl
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-generation.ttl
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-usage.ttl
-curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-simple-without-comments.ttl
-
-for ttl in `find . -name "*.ttl" -newer beforefetch`; do
-   cat $ttl | sed 's/</\&lt;/g; s/>/\&gt;/g' > b
-   mv b $ttl
-done
-rm beforefetch
+mkdir includes
+pushd includes
+   touch beforefetch
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
+   # Examples
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-association.ttl
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-derivation.ttl
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-generation.ttl
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-usage.ttl
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-simple-without-comments.ttl
+   for ttl in `find . -name "*.ttl" -newer beforefetch`; do
+      cat $ttl | sed 's/</\&lt;/g; s/>/\&gt;/g' > b
+      mv b $ttl
+   done
+   rm beforefetch
+popd
