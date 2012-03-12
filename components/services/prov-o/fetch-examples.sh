b37cd07cbb874994207fc179b276fbeafebe1c5f
Tim L <timrdf@gmail.com>
2012-03-12 11:40:07 -0400

    wd4

--- components/services/prov-o/fetch-examples.sh
+++ components/services/prov-o/fetch-examples.sh
@@ -1,6 +1,19 @@
+pushd ../../../static/prov-o
+   # Diagrams
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/core.png
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/Qualified-Association.png
+   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/involvements.png
+popd
+
 touch beforefetch
 curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
-for ttl in `find . -newer beforefetch`; do
+# Examples
+curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-association.ttl
+curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-derivation.ttl
+curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-generation.ttl
+curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-usage.ttl
+
+for ttl in `find . -name "*.ttl" -newer beforefetch`; do
    cat $ttl | sed 's/</\&lt;/g; s/>/\&gt;/g' > b
    mv b $ttl
 done
