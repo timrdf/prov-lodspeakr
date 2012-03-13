84ae410fe54b0ab6b4fdbb887ec44d8f77c4f2a5
Tim L <timrdf@gmail.com>
2012-03-13 15:08:45 -0400

    switched to LODE styling

--- components/services/prov-o/fetch-examples.sh
+++ components/services/prov-o/fetch-examples.sh
@@ -1,36 +1,42 @@
-mkdir ../../../static/prov-o-diagrams
+if [ ! -d ../../../static/prov-o-diagrams ]; then
+   mkdir ../../../static/prov-o-diagrams
+fi
 pushd ../../../static/prov-o-diagrams
    # Diagrams
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/core.png
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/Qualified-Association.png
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/involvements.png
-popd
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/core.png
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/Qualified-Association.png
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/involvements.png
+popd &> /dev/null
 
-mkdir ../../../static/prov-o-lode
+if [ ! -d ../../../static/prov-o-lode ]; then
+   mkdir ../../../static/prov-o-lode
+fi
 pushd ../../../static/prov-o-lode
-   curl -O 'http://speronitomcat.web.cs.unibo.it:8080/LODE/owl.css'
-   curl -O 'http://speronitomcat.web.cs.unibo.it:8080/LODE/Primer.css'
-   curl -O 'http://speronitomcat.web.cs.unibo.it:8080/LODE/rec.css'
-   curl -O 'http://speronitomcat.web.cs.unibo.it:8080/LODE/extra.css'
-popd
+   curl -sO 'http://speronitomcat.web.cs.unibo.it:8080/LODE/owl.css'
+   curl -sO 'http://speronitomcat.web.cs.unibo.it:8080/LODE/Primer.css'
+   curl -sO 'http://speronitomcat.web.cs.unibo.it:8080/LODE/rec.css'
+   curl -sO 'http://speronitomcat.web.cs.unibo.it:8080/LODE/extra.css'
+popd &> /dev/null
 
-mkdir includes
+if [ ! -d includes ]; then
+   mkdir includes
+fi
 pushd includes
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/default/ontology/ProvenanceOntology.owl
-   .././cross-reference.py http://dvcs.w3.org/hg/prov/raw-file/default/ontology/ProvenanceOntology.owl ../ProvenanceOntology.owl prov
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/default/ontology/ProvenanceOntology.owl
+   .././cross-reference.py http://dvcs.w3.org/hg/prov/raw-file/default/ontology/ProvenanceOntology.owl ProvenanceOntology.owl prov
    rm ProvenanceOntology.owl
  
    touch beforefetch
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/examples/eg-9-provrdf-owl-coverage/rdf/create/rdf/eg-9-provrdf-owl-coverage.html.ttl
    # Examples
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-association.ttl
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-derivation.ttl
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-generation.ttl
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-usage.ttl
-   curl -O http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-simple-without-comments.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-association.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-derivation.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-generation.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-qualified-usage.ttl
+   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/khalid-jun-dropbox/eg16-journalism-simple-without-comments.ttl
    for ttl in `find . -name "*.ttl" -newer beforefetch`; do
       cat $ttl | sed 's/</\&lt;/g; s/>/\&gt;/g' > b
       mv b $ttl
    done
    rm beforefetch
-popd
+popd &> /dev/null
