0ce4aecfc1dfdd05d5e7160eeb8c205310e1141f
Tim L <timrdf@gmail.com>
2012-04-27 10:32:18 -0400

    image url refs

--- components/services/prov-o/prepare-materials.sh
+++ components/services/prov-o/prepare-materials.sh
@@ -24,9 +24,9 @@ if [ ! -d prov-o-html-sections ]; then
    mkdir prov-o-html-sections
 fi
 pushd prov-o-html-sections
-#   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-starting-points.inc.html
+    curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-starting-points.inc.html
     curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-expanded-terms.inc.html
-#   curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-qualified-terms.inc.html
+    curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-qualified-terms.inc.html
     curl -sO http://dvcs.w3.org/hg/prov/raw-file/tip/ontology/prov-o-html-sections/description-collections.inc.html
 popd &> /dev/null
 
