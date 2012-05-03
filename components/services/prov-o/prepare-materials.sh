02e3ccf6690a05310f5e17d54f72126f6ecc5adf
Tim L <timrdf@gmail.com>
2012-05-03 13:37:07 -0400

    added changeset grab

--- components/services/prov-o/prepare-materials.sh
+++ components/services/prov-o/prepare-materials.sh
@@ -40,10 +40,13 @@ pushd includes
       pushd prov &> /dev/null
          hg pull
          hg update
+         changeset=`hg log | head -1 | sed 's/^.*://'`
       popd &> /dev/null
    fi
 
+   echo $changeset > changeset.txt
    owlURLversion="http://dvcs.w3.org/hg/prov/raw-file/default/ontology/ProvenanceOntology.owl"
+   owlURLversion="http://dvcs.w3.org/hg/prov/raw-file/$changeset/ontology/ProvenanceOntology.owl"
    #owlURLversion="http://dvcs.w3.org/hg/prov/raw-file/cf7deb6c5f3e/ontology/ProvenanceOntology.owl"
    curl -sO $owlURLversion
    .././cross-reference.py $owlURLversion ProvenanceOntology.owl prov
